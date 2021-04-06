8ab6e2e0005a2bf9aa661c79e9dd5ad60401f000
hong <juhong919@gmail.com>
2021-04-06 17:52:36 +0900

    랜덤모듈

---
+++ DemoRandom.py
@@ -0,0 +1,30 @@
+# DemoRandom.py
+
+# 임의의 숫자 만들기
+import random
+
+# 0.0 ~ 1.0 사이의 값
+print(random.random())
+print(random.random())
+# 2.0 ~ 5.0 사이의 값
+print(random.uniform(2.0, 5.0))
+
+# 임의의 정수 만들기
+print([random.randrange(20) for i in range(10)])
+print([random.randrange(20) for i in range(10)])
+
+# 유일한 값이 생성
+print(random.sample(range(20),10))
+print(random.sample(range(20),10))
+
+# 로또 번호
+lotto = list(range(1,46))
+# 섞여있지 않음
+print(lotto)
+
+# 랜덤하게 섞기
+random.shuffle(lotto) 
+print(lotto)
+
+for item in range(5):
+    print(lotto.pop())
