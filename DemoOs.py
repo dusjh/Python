79dcdab47754baea9e02bde8b902de3854e14632
hong <juhong919@gmail.com>
2021-04-06 18:18:01 +0900

    운영체제 파일

---
+++ DemoOs.py
@@ -0,0 +1,28 @@
+# DemoOs.py
+
+from os.path import *
+
+# print(dir(os.path))
+print(abspath("python.exe"))
+print(basename("c:\\python38\\python.exe"))
+print(exists("c:\\python38\\python.exe"))
+print(getsize("c:\\python38\\python.exe"))
+
+# 운영체제에 있는 명령어를 실행
+from os import *
+
+# 경로 이동 확인함 (c:\work2 -> c: -> c:\work2)
+print("현재 작업 폴더: ",getcwd())
+chdir("..")
+chdir("c:\\work2")
+print("현재 폴더: ", getcwd())
+
+# 외부에 실행파일 수행
+system("notepad.exe")  # 메모장 실행
+
+# 파일, 폴더 리스트
+import glob
+print(glob.glob("*.py"))
+print("="*20)
+for item in glob.glob("*.*"):
+    print(item)
